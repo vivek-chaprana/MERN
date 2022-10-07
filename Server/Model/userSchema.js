aeded0ea6b5e0b48c1e72d270c242db2866cb6f6
Vivek Chaprana <vivek2003ji@gmail.com>
2022-10-07 21:45:03 +0530

    Securing Pass And Generating JWT

--- Server/Model/userSchema.js
+++ Server/Model/userSchema.js
@@ -1,32 +1,67 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const useSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: Number,
-        required: true,
-    },
-    work: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    cpassword: {
-        type: String,
-        required: true,
-    },
+//Defining user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token:{
+        type:String,
+        required:true
+      }
+    }
+  ]
 });
 
-const User = mongoose.model("USER", useSchema);
+
+// Hashing the password
+userSchema.pre('save', async function(next) {  
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    }
+    next();
+});
+
+//Generating JWT 
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({_id:this._id}, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({token : token});
+    await this.save();
+    return token;
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Creating collection
+const User = mongoose.model("USER", userSchema);
 
 module.exports = User;
--- Server/Router/auth.js
+++ Server/Router/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 require("../db/connection");
 const User = require("../Model/userSchema");
 
@@ -21,30 +24,44 @@ router.post("/register", async (req, res) => {
   try {
     const userExists = await User.findOne({ email: email });
     if (userExists) return res.status(409).send("User already exists");
-
-    const user = new User({ name, email, phone, work, password, cpassword });
-    await user.save();
-
-    res.status(201).send("User registered successfully.");
+    else if (password != cpassword)
+      return res
+        .status(422)
+        .send("Error : Password and confirm password do not match");
+    else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+      await user.save();
+      res.status(201).send("User registered successfully.");
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Failed to register user, Please try again later.");
   }
 });
 
-router.post('/signin',async (req, res) => {
-    try {
-        const {email , password} = req.body;
-        if(!email || !password) res.status(403).send("Please enter all the fields");
-        const userLogin = await User.findOne({ email: email})
-        userLogin ? res.send("Login Sucessfull") : res.status(401).send("Invalid details")
-        console.log(userLogin)
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send("Failed to Sign In, Please try again later.");
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      res.status(403).send("Please enter all the fields");
+    const userLogin = await User.findOne({ email: email });
+    if(userLogin){
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+      let token = await userLogin.generateAuthToken();
+      res.cookie("jwtoken",token,{
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly:true
+      })
+      isMatch
+      ? res.send("Login Sucessfull")
+      : res.status(401).send("Invalid Credentials")
+    }else{
+      res.status(401).send("Invalid Credentials")
     }
-})
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Failed to Sign In, Please try again later.");
+  }
+});
 
 module.exports = router;
